a7a8a53d07328f661b5564e205c4fb623a6e9696
Sharon Zou <szou00@stuy.edu>
2019-03-19 22:32:34 -0400

    deleted unnecessary code

--- Merge.java
+++ Merge.java
@@ -62,45 +62,6 @@ public class Merge {
     }
     System.out.println(Arrays.toString(newary));
   }
-    // if (i>=j) {
-    //   return;
-    //   // temp[i] = data[j];
-    //   // temp[j] = data[i];
-    // }
-    // mergesort(temp,data,i,j/2);
-    // mergesort(temp,data,j/2+1,j);
-    // int ic = 0; int jc = j/2+1;int x =0;
-    // System.out.println("first array length: " + (j/2+1) + " second: " + (j+1));
-    // while (ic != j/2+1) {
-    //   System.out.println("ic: " +  ic + " jc: " + jc);
-    //   System.out.println(ic == j/2+1);
-    //   if (data[ic] >= data[jc]) {
-    //     System.out.println("x: " + x);
-    //     System.out.println("index of first array: " + ic);
-    //     temp[x] = data[ic];
-    //     ic+=1;
-    //     x+=1;
-    //   }
-    //   else {
-    //     System.out.println("x: " + x);
-    //     System.out.println("index of second array: " + ic);
-    //     temp[x] = data[jc];
-    //     jc+=1;
-    //     x+=1;
-    //   }
-    // }
-    // if (ic == j/2+1) {
-    //   for (int y = jc;y<j+1;y++) {
-    //     temp[x] = data[y];
-    //     x+=1;
-    //   }
-    // }
-    // if (jc == j+1) {
-    //   for (int y = ic;y<j/2+1;y++) {
-    //     temp[x] = data[y];
-    //     x+=1;
-    //   }
-    // }
 
   public static void main(String args[]) {
     int[] data1 = {38,27,43,3,9,82,10};
