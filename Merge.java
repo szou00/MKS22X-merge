0bc69f83ffbbfe13c65f0913680d0046d5a1e312
Sharon Zou <szou00@stuy.edu>
2019-03-20 19:44:49 -0400

    time printed out from k's main seems to be right

--- Merge.java
+++ Merge.java
@@ -40,7 +40,6 @@ public class Merge {
   public static void merge(int[] data, int[] beg, int[] end) {
     int b = 0; int e = 0; //keeps track of the indexes in both arrays
     int x = 0;
-    // data = new int[data.length];
     // System.out.println("b: " + b + " e: " + e);
     while (b!=beg.length && e != end.length) {
       if (beg[b] <= end[e]) {
@@ -67,14 +66,42 @@ public class Merge {
     }
     // System.out.println(Arrays.toString(beg));
     // System.out.println(Arrays.toString(end));
-    System.out.println(Arrays.toString(data));
+    // System.out.println(Arrays.toString(data));
   }
 
-  public static void main(String args[]) {
-    int[] data1 = {38,27,43,3,9,82,10};
-
-    mergesort(data1);
-    System.out.println(Arrays.toString(data1));
+  public static void main(String[]args){
+  System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
+  int[]MAX_LIST = {1000000000,500,10};
+  for(int MAX : MAX_LIST){
+    for(int size = 31250; size < 2000001; size*=2){
+      long qtime=0;
+      long btime=0;
+      //average of 5 sorts.
+      for(int trial = 0 ; trial <=5; trial++){
+        int []data1 = new int[size];
+        int []data2 = new int[size];
+        for(int i = 0; i < data1.length; i++){
+          data1[i] = (int)(Math.random()*MAX);
+          data2[i] = data1[i];
+        }
+        long t1,t2;
+        t1 = System.currentTimeMillis();
+        Merge.mergesort(data2);
+        t2 = System.currentTimeMillis();
+        qtime += t2 - t1;
+        t1 = System.currentTimeMillis();
+        Arrays.sort(data1);
+        t2 = System.currentTimeMillis();
+        btime+= t2 - t1;
+        if(!Arrays.equals(data1,data2)){
+          System.out.println("FAIL TO SORT!");
+          System.exit(0);
+        }
+      }
+      System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+    }
+    System.out.println();
   }
+}
 
 }
