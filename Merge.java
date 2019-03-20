c1d80005f251037e4faeff9750e921e919c836be
Sharon Zou <szou00@stuy.edu>
2019-03-19 23:51:22 -0400

    won't sort completely

--- Merge.java
+++ Merge.java
@@ -16,48 +16,58 @@ public class Merge {
     }
 
     //initializing left array
-    int[] beg = new int[data.length/2];
+    int[] beg = new int[data.length-data.length/2];
     for (int i = 0; i < beg.length; i++) {
       beg[i] = data[i];
     }
 
     //initializing right array
-    int[] end = new int[data.length-data.length/2];
+    int[] end = new int[data.length/2];
     for (int i = 0; i<end.length;i++) {
-      end[i] = data[i+data.length/2];
+      end[i] = data[i+(data.length-data.length/2)];
     }
 
+    // System.out.print("beg: ");
+    // System.out.println(Arrays.toString(beg));
+    // System.out.print("end: ");
+    // System.out.println(Arrays.toString(end));
     mergesort(beg,0,beg.length-1);
     mergesort(end,beg.length,data.length-1);
 
+    merge(data,beg,end);
+  }
+
+  public static void merge(int[] data, int[] beg, int[] end) {
     int b = 0; int e = 0; //keeps track of the indexes in both arrays
     int x = 0;
-    int[] newary = new int[data.length];
+    data = new int[data.length];
     // System.out.println("b: " + b + " e: " + e);
     while (b!=beg.length && e != end.length) {
       if (beg[b] <= end[e]) {
         // System.out.println(beg[b] + " " + end[e]);
-        newary[x] = beg[b];
+        data[x] = beg[b];
         b+=1;
         x+=1;
       }
       else {
-        newary[x] = end[e];
+        data[x] = end[e];
         e+=1;
         x+=1;
       }
     }
     while (b!= beg.length) {
-      newary[x] = beg[b];
+      data[x] = beg[b];
       b+=1;
+      x+=1;
       }
     while (e!= end.length) {
-      newary[x] = end[e];
+      data[x] = end[e];
       e+=1;
+      x+=1;
     }
-    System.out.println(Arrays.toString(beg));
-    System.out.println(Arrays.toString(end));
-    System.out.println(Arrays.toString(newary));
+    // System.out.println(Arrays.toString(beg));
+    // System.out.println(Arrays.toString(end));
+    System.out.println(Arrays.toString(data));
   }
 
   public static void main(String args[]) {
